1ebb6e331f0d8cd99fb7314b13037c92202f06e7
andre-menezes <andre.menezes@vnda.com.br>
2021-03-13 16:33:35 -0300

    Criando o componente pageWrapper

---
+++ src/components/wrappers/WebsitePage/hoc/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WebsitePageWrapper from '..'
+import WebsiteGlobalProvider from '../provider'
+
+export default function websitePageHOC(PageComponent, { pageWrapperProps }) {
+  return props => (
+    <WebsiteGlobalProvider>
+      <WebsitePageWrapper {...pageWrapperProps}>
+        <PageComponent {...props} />
+      </WebsitePageWrapper>
+    </WebsiteGlobalProvider>
+  )
+}
