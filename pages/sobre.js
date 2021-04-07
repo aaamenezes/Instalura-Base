fca46a98589688d7181f25b3e0fe66f588f69364
andre-menezes <andre.menezes@vnda.com.br>
2021-04-06 22:57:04 -0300

    Conectar com DatoCMS e segunrança do token

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { GraphQLClient, gql } from 'graphql-request'
+import AboutScreen from '../src/components/screens/AboutScreen'
+import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc'
 
-export default function PageSobre() {
-  return (
-    <div>
-      Página Sobre
-    </div>
-  )
+export async function getStaticProps() {
+  // Ta vindo do arquivo .env.local
+  const TOKEN = process.env.DATO_CMS_TOKEN
+  const DatoCMSURL = 'https://graphql.datocms.com/'
+
+  const client = new GraphQLClient(DatoCMSURL, {
+    headers: {
+      Authorization: `Bearer ${ TOKEN }`
+    }
+  })
+
+  const query = gql`
+    query {
+      aboutPage {
+        pageTitle
+        pageDescription
+      }
+    }
+  `
+
+  const messages = await client.request(query)
+
+  return {
+    props: {
+      messages
+    }
+  }
 }
+
+export default websitePageHOC(AboutScreen)
---
+++ src/components/screens/AboutScreen/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Box } from '../../foundation/layout/Box'
+import { Grid } from '../../foundation/layout/Grid'
+import Text from '../../foundation/Text'
+
+function AboutScreen({ messages }) {
+  return (
+    <Box
+      display='flex'
+      flexDirection='column'
+      flex={1}
+    >
+      <Grid.Container>
+        <Grid.Row
+          marginTop={{ xs: '32px', md: '120px' }}
+          flex='1'
+        >
+          <Grid.Col
+            value={{ xs: 12, md: 6, lg: 6 }}
+            offset={{ md: 2 }}
+            flex={1}
+          >
+            <Text
+              variant='title'
+              tag='h2'
+              color='vdfmvdfmvkm'
+            >
+              {messages.aboutPage.pageTitle}
+            </Text>
+            <Box>
+              {messages.aboutPage.pageDescription}
+            </Box>
+            {/* Caso o Next não interprete o HTML vindo da API */}
+            {/* Pode fazer assim: */}
+            {/* <Box
+              dangerouslySetInnerHTML={{
+                __html: messages.aboutPage.pageDescription
+              }}
+            /> */}
+          </Grid.Col>
+        </Grid.Row>
+      </Grid.Container>
+    </Box>
+  )
+}
+
+AboutScreen.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  messages: PropTypes.object.isRequired
+}
+
+export default AboutScreen
